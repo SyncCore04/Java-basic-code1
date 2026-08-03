3f5136d17930bf0e5a828602eb027f66e446e0c4
SyncCore04 <zhanglay2004@gmail.com>
2026-08-04 00:48:28 +0800

    for(数列和)

---
+++ src/main/java/ForDemo3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ForDemo3 {
+    public static void main(String[] args) {
+        /*S(4)=1-2+3-4=-2*/
+        int Sn=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入数字");
+        int num=sc.nextInt();
+        for(int i=1;i<=num;i++){
+            if(i%2==0){
+                Sn-=i;
+            }else{
+                Sn+=i;
+            }
+        }
+        System.out.println("数列的和为："+Sn);
+    }
+}
