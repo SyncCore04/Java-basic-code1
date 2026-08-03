a3ec93324a6e7138fcb9b56bff5e01d559b8282c
SyncCore04 <zhanglay2004@gmail.com>
2026-08-04 00:42:17 +0800

    for(偶数和)

---
+++ src/main/java/ForDemo2.java
@@ -0,0 +1,12 @@
+public class ForDemo2 {
+    public static void main(String[] args) {
+        /*100以内偶数和*/
+        int sum = 0;
+        for(int i=1;i<=100;i++){
+            if(i%2==0){
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
