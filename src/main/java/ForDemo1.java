4e522e742661e148b1ef6a2e44216a5da5e5f294
SyncCore04 <zhanglay2004@gmail.com>
2026-08-04 00:34:18 +0800

    for(斐波那契)

---
+++ src/main/java/ForDemo1.java
@@ -0,0 +1,17 @@
+public class ForDemo1 {
+    public static void main(String[] args) {
+        /*有一组特殊的数字，从第三项开始，每一项都是前两项的数字和，请问第10项的数字是多少？
+        0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89.…*/
+        int a=0;
+        int b=1;
+        int target=10;
+        System.out.print(1+",");
+        for(int i=0;i<target;i++){
+            int c=a+b;
+            a=b;
+            b=c;
+            System.out.print(c);
+            System.out.print(i==target-1?"\n":",");
+        }
+    }
+}
