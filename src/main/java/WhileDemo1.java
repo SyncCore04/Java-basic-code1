df271c480c86cdc65f35aab517b264e55735da40
SyncCore04 <zhanglay2004@gmail.com>
2026-08-04 00:51:50 +0800

    While(利滚利)

---
+++ src/main/java/WhileDemo1.java
@@ -0,0 +1,12 @@
+public class WhileDemo1 {
+    public static void main(String[] args) {
+        /*100000元存银行，复利1.7%,需要多少年可以实现本金翻倍*/
+        double money=100000;
+        double year=0;
+        while(money<=200000){
+            money=money*1.017;
+            year++;
+        }
+        System.out.println("需要"+year+"年可以实现本金翻倍");
+    }
+}
