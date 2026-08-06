7269e206f261b78f5141b1212a4e546e4a9dc70d
SyncCore04 <zhanglay2004@gmail.com>
2026-08-06 21:42:18 +0800

    方法的初步使用

--- src/main/java/method/MethodDemo1.java
+++ src/main/java/method/MethodDemo1.java
@@ -1,13 +1,23 @@
 package method;
 
+import java.util.Scanner;
+
 public class MethodDemo1 {
     // 方法定义
     public static int getSum(int a ,int b){
         return a+b;
     }
+
+    // 方法调用
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        System.out.println(getSum(a,b));
+        int a,b;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter a number");
+        a = input.nextInt();
+        System.out.println("enter b number");
+        b = input.nextInt();
+
+        System.out.println("a+b = "+getSum(a,b));
     }
 }
