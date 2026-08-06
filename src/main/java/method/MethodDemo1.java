1310d86c2b93d9c46f8bd017c93779bb03abf18f
SyncCore04 <zhanglay2004@gmail.com>
2026-08-06 21:37:16 +0800

    方法的定义

--- src/main/java/method/MethodDemo1.java
+++ src/main/java/method/MethodDemo1.java
@@ -1,7 +1,13 @@
 package method;
 
 public class MethodDemo1 {
+    // 方法定义
+    public static int getSum(int a ,int b){
+        return a+b;
+    }
     public static void main(String[] args) {
-
+        int a = 10;
+        int b = 20;
+        System.out.println(getSum(a,b));
     }
 }
