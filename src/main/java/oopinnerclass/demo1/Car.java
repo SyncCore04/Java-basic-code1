ca0181223834dba8ac123e726d43753e105eb11b
Apollo <zhanglay2004@gmail.com>
2026-08-28 20:35:11 +0800

    内部类

---
+++ src/main/java/oopinnerclass/demo1/Car.java
@@ -0,0 +1,12 @@
+package oopinnerclass.demo1;
+
+public class Car {
+    String carBrand;
+    int carAge;
+    String carColor;
+
+    class Engine{
+        String engineBrand;
+        int engineAge;
+    }
+}
---
+++ src/main/java/oopinnerclass/demo1/Test.java
@@ -0,0 +1,7 @@
+package oopinnerclass.demo1;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
